f8228ec1b6faf72c6e040309ca7e71be1c097237
Phonkd <phonkd@phonkd.net>
2026-04-02 00:26:57 +0200

    Update android.nix

--- modules/hosts/android.nix
+++ modules/hosts/android.nix
@@ -6,6 +6,7 @@
 }:
 {
   flake.nixOnDroidConfigurations."android" = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+    pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
     modules = [
       {
         home-manager.config = {pkgs, ...}: {
