973b3a8a0f5e2c5562e833ecb862157efbc364f4
Phonkd <phonkd@phonkd.net>
2026-04-03 14:57:51 +0200

    add sops to servers

--- modules/hosts/types/server/default.nix
+++ modules/hosts/types/server/default.nix
@@ -8,6 +8,7 @@
   flake.nixosModules.server-nixos = {
     imports = [
       self.nixosModules.server-teleport
+      inputs.sops-nix.nixosModules.sops
       #(modulesPath + "/profiles/qemu-guest.nix")
     ];
   };
