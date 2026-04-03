a96ff8d1e328888be28e293ef4323cabc38fedab
Phonkd <phonkd@phonkd.net>
2026-04-03 17:45:52 +0200

    fix sops module

--- modules/hosts/types/server/default.nix
+++ modules/hosts/types/server/default.nix
@@ -9,6 +9,7 @@
     imports = [
       self.nixosModules.server-teleport
       inputs.sops-nix.nixosModules.sops
+      self.nixosModules.server-sops
       #(modulesPath + "/profiles/qemu-guest.nix")
     ];
   };
