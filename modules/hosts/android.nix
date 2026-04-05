415c9e522058febadf8e9b66108c706469f4ebf6
Phonkd <phonkd@phonkd.net>
2026-04-05 17:32:28 +0200

    add awk to nixondroid to fix fzf history

--- modules/hosts/android.nix
+++ modules/hosts/android.nix
@@ -8,21 +8,27 @@
     pkgs = import inputs.nixpkgs-unstable-droid { system = "aarch64-linux"; };
     home-manager-path = inputs.home-manager.outPath;
     modules = [
-      ({pkgs, ...}: {
-        system.stateVersion = "24.05";
-        home-manager.config = {pkgs, lib, ...}: {
-          imports = [ self.homeModules.base ];
-          home.username = lib.mkForce "nix-on-droid";
-          # home.packages = with pkgs; [
-          #   openssh
-          # ];
-          #home.stateVersion = lib.mkForce "24.05";
-        };
-        user.shell = pkgs.zsh;
-        environment.packages = with pkgs; [
-          openssh
-        ];
-      })
+      (
+        { pkgs, ... }:
+        {
+          system.stateVersion = "24.05";
+          home-manager.config =
+            { pkgs, lib, ... }:
+            {
+              imports = [ self.homeModules.base ];
+              home.username = lib.mkForce "nix-on-droid";
+              # home.packages = with pkgs; [
+              #   openssh
+              # ];
+              #home.stateVersion = lib.mkForce "24.05";
+            };
+          user.shell = pkgs.zsh;
+          environment.packages = with pkgs; [
+            openssh
+            awk
+          ];
+        }
+      )
     ];
 
   };
