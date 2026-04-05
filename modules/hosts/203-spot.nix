27071cc96f782332fe53ff7117086e9ecff8e97a
Phonkd <phonkd@phonkd.net>
2026-04-05 17:38:06 +0200

    faster gc on 203

--- modules/hosts/203-spot.nix
+++ modules/hosts/203-spot.nix
@@ -32,5 +32,10 @@
       networking.firewall.allowedTCPPorts = [
         22
       ];
+      nix.gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 3d";
+      };
     };
 }
