0e4f1bbe35c770a4dd9a53deb12e20cfac2ab9e5
Phonkd <phonkd@phonkd.net>
2026-05-26 00:34:00 +0200

    slimenzb

--- modules/hosts/types/server/oldblac-vm.nix
+++ modules/hosts/types/server/oldblac-vm.nix
@@ -14,6 +14,7 @@
       fileSystems."/" = {
         device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
         fsType = "ext4";
+        autoResize = true;
       };
 
       fileSystems."/boot" = {
