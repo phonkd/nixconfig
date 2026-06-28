9ee7bc528ebf5b312904b3f13f26e7f29965f006
Phonkd <phonkd@phonkd.net>
2026-06-28 15:58:48 +0200

    add alloy

--- modules/observability.nix
+++ modules/observability.nix
@@ -244,10 +244,76 @@
       pkgs,
       lib,
       noughtyLib,
+      config,
       ...
     }:
     lib.mkIf (noughtyLib.hostHasTag "observability-sender") {
-      # TODO: configure Alloy / Promtail / Prometheus remote_write targeting
-      #       the observability server over the VPN (10.9.0.1).
+      services.alloy.enable = true;
+      # systemd.services.alloy.serviceConfig = {
+      #     # Alloy normally runs as an unprivileged user; force root instead.
+      #   User  = lib.mkForce "root";
+      #   Group = lib.mkForce "root";  # optional, but usually pair with User
+      # };
+      services.prometheus.exporters.node = {
+        # enabledCollectors = [
+        #   "node"
+        #   "postgres"
+        # ];
+        enable = true;
+      };
+
+      environment.etc."alloy/config.alloy" = {
+        text =
+          let
+            hostname = config.networking.hostName;
+            lokiendpoint = "http://10.9.0.1:3100/insert/loki/api/v1/push";
+            mimirendpoint = "http://10.9.0.1:9009/api/v1/write";
+          in
+          ''
+            prometheus.exporter.unix "gagu" { }
+
+            // Configure a prometheus.scrape component to collect unix metrics.
+            prometheus.scrape "gagu" {
+              targets    = prometheus.exporter.unix.gagu.targets
+              forward_to = [prometheus.remote_write.nixvms.receiver]
+            }
+
+            prometheus.remote_write "nixvms" {
+              external_labels = {
+                hostname = "${hostname}",
+                instance = "${hostname}",
+              }
+              endpoint {
+                url = "${mimirendpoint}"
+                remote_timeout = "10s"
+              }
+            }
+            loki.relabel "journal" {
+              forward_to = []
+
+              rule {
+                source_labels = ["__journal__systemd_unit"]
+                target_label  = "unit"
+              }
+            }
+
+            loki.source.journal "read"  {
+              forward_to    = [loki.write.endpoint.receiver]
+              relabel_rules = loki.relabel.journal.rules
+              labels        = {
+                component = "loki.source.journal",
+              }
+            }
+
+            loki.write "endpoint" {
+              external_labels = {
+                hostname = "${hostname}",
+              }
+              endpoint {
+                url = "${lokiendpoint}"
+              }
+            }
+          '';
+      };
     };
 }
